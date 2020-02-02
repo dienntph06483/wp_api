fef148356f03ea403283913c97c10a6c0aa14dfd
dien <dienntph06483@fpt.edu.vn>
2020-02-02 14:58:35 +0700

    dadsa 3

--- index.php
+++ index.php
@@ -97,7 +97,7 @@ include_once 'config.php';
                             <div class="form-group">
                                 <label class="control-label col-sm-2" for="description">Description:</label>
                                 <div class="col-sm-10">
-                                    <textarea class="form-control" name="content" id="content" placeholder="Enter description haha"></textarea>
+                                    <textarea class="form-control" name="content" id="content" placeholder="Enter description haha 2"></textarea>
                                 </div>
                             </div>
                             <div class="form-group"> 
